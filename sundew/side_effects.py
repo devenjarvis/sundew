69bce762d2b7032f6fc3d82f84af81b2f4972f4a
devenjarvis <devenjarvis@gmail.com>
2023-03-18 13:25:51 -0400

    feat: Add better support for all side_effect operators

--- sundew/side_effects.py
+++ sundew/side_effects.py
@@ -4,17 +4,52 @@ from collections.abc import Callable
 from typing import Any
 
 
+def generate_inverse_operation_str(  # noqa: C901, PLR0911
+    operation: ast.cmpop,
+) -> str:
+    if isinstance(operation, ast.Eq):
+        return "!="
+    if isinstance(operation, ast.NotEq):
+        return "=="
+
+    if isinstance(operation, ast.Lt):
+        return ">="
+    if isinstance(operation, ast.GtE):
+        return "<"
+
+    if isinstance(operation, ast.LtE):
+        return ">"
+    if isinstance(operation, ast.Gt):
+        return "<="
+
+    if isinstance(operation, ast.Is):
+        return "is not"
+    if isinstance(operation, ast.IsNot):
+        return "is"
+
+    if isinstance(operation, ast.In):
+        return "is not in"
+    if isinstance(operation, ast.NotIn):
+        return "is in"
+    return "unknown operator"
+
+
 # Generate assert statement from a Compare node
 def generate_compare_assert(node: ast.Compare) -> ast.Assert:
     return ast.Assert(
         test=node,
         msg=ast.JoinedStr(
             values=[
-                ast.Constant(value="left="),
+                ast.Constant(
+                    value=f"Side-effect '{ast.unparse(node)}' failed because: "
+                ),
+                ast.Constant(value=f"{ast.unparse(node.left)} == "),
                 ast.FormattedValue(value=node.left, conversion=-1),
-                ast.Constant(value=" right="),
+                ast.Constant(value=", and "),
+                ast.FormattedValue(value=node.left, conversion=-1),
+                ast.Constant(value=f" {generate_inverse_operation_str(node.ops[0])} "),
                 ast.FormattedValue(value=node.comparators[0], conversion=-1),
-            ],
+            ]
         ),
     )
 
--- tests/examples/test_simple_example.py
+++ tests/examples/test_simple_example.py
@@ -19,3 +19,10 @@ test(simple_example.simple_sleep)(
     kwargs={"secs": 2},
     returns=None,
 )
+
+
+test(simple_example.quadratic)(
+    kwargs={"a": 1, "b": -8, "c": 5},
+    returns=(7.3166247903554, 0.6833752096446002),
+    side_effects=[lambda _: _.a < 5],  # noqa: PLR2004
+)
