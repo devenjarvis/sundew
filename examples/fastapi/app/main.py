6fe89cf537e04e81e4b4841400ce362b7224221f
devenjarvis <devenjarvis@gmail.com>
2023-04-14 20:00:37 -0400

    feat: fastapi_example working with ormar

--- examples/fastapi/app/main.py
+++ examples/fastapi/app/main.py
@@ -3,22 +3,12 @@
 
 from typing import Any
 
-import databases
-import sqlalchemy
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from app.db import Notes, database
+
 app = FastAPI()
-
-# Setup db
-DATABASE_URL = "sqlite:///./fastapi_example.db"
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
-
-engine = sqlalchemy.create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-metadata.create_all(engine)
 
 
 # Models
@@ -27,30 +17,23 @@ class NoteIn(BaseModel):
     completed: bool
 
 
-class Note(BaseModel):
-    id: int  # noqa: A003
-    text: str
-    completed: bool
-
-
 @app.on_event("startup")
 async def startup() -> None:
-    await database.connect()
+    if not database.is_connected:
+        await database.connect()
 
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
-    await database.disconnect()
+    if database.is_connected:
+        await database.disconnect()
 
 
-@app.get("/notes/", response_model=list[Note])
-async def read_notes() -> list[databases.interfaces.Record]:
-    query = notes.select()
-    return await database.fetch_all(query)
+@app.get("/notes/", response_model=list[Notes])
+async def read_notes() -> list[Notes]:
+    return await Notes.objects.select_all().all()
 
 
-@app.post("/notes/", response_model=Note)
+@app.post("/notes/", response_model=Notes)
 async def create_note(note: NoteIn) -> dict[str, Any]:
-    query = notes.insert().values(text=note.text, completed=note.completed)
-    last_record_id = await database.execute(query)
-    return {**note.dict(), "id": last_record_id}
+    return await Notes.objects.create(text=note.text, completed=note.completed)
