5729b5014c4bf6c358af7c0d87e291598a53f228
devenjarvis <devenjarvis@gmail.com>
2023-04-22 21:10:12 -0400

    fix: lint

--- examples/boto3/example/s3.py
+++ examples/boto3/example/s3.py
@@ -2,7 +2,7 @@
 # Source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-examples.html
 
 import logging
-import os
+from pathlib import Path
 
 import boto3
 from botocore.exceptions import ClientError
@@ -11,19 +11,19 @@ from botocore.exceptions import ClientError
 def upload_file(file_name: str, bucket: str, object_name: str | None = None) -> None:
     # If S3 object_name was not specified, use file_name
     if object_name is None:
-        object_name = os.path.basename(file_name)
+        object_name = Path(file_name).name
 
     # Upload the file
     s3_client = boto3.client("s3")
     try:
         s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
+    except ClientError:
+        logging.exception()
         return False
     return True
 
 
-def download_file(bucket_name, object_name, file_name):
+def download_file(bucket_name: str, object_name: str, file_name: str) -> None:
     s3 = boto3.client("s3")
-    with open(file_name, "wb") as f:
+    with Path(file_name).open("wb") as f:
         s3.download_fileobj(bucket_name, object_name, f)
